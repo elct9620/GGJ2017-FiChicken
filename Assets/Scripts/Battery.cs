5513751f892937bf0b3fde38f0c0664dad577e33
蒼時弦也 <elct9620@frost.tw>
2017-01-21 19:15:12 +0800

    Add eatable item

---
+++ Assets/Scripts/Battery.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour {
+
+	public enum SkinType {
+		Battery
+	}
+	public int Power = 1;
+	public SkinType Skin = SkinType.Battery;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void OnValidate() {
+		SetupSkin();
+	}
+
+	void SetupSkin() {
+		GetComponent<SpriteRenderer>().sprite = GetSprite();
+	}
+
+	Sprite GetSprite() {
+		// Resource searched by name
+		string ResourceName = string.Format("Sprites/Item/{0}", Skin.ToString("G"));
+		return Resources.Load<Sprite>(ResourceName);
+	}
+
+	void OnTriggerEnter2D() {
+		Destroy(gameObject);
+		// TODO: Call player charge API
+	}
+
+}
