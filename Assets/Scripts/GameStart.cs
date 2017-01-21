8b4f895a975378bcd1c0c7d7feb42d405ceeb913
蒼時弦也 <elct9620@frost.tw>
2017-01-22 06:15:56 +0800

    Add game start scene

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UniRx;
+
+public class GameStart : MonoBehaviour {
+
+	void Start () {
+		GetComponent<Button>()
+			.OnClickAsObservable()
+			.Subscribe( _ => { StartGame(); } )
+			.AddTo(this);
+	
+	}
+	
+	void StartGame () {
+		Random.InitState(System.DateTime.Now.Millisecond);
+		int StageID = Random.Range(1, 5);
+		SceneManager.LoadScene(string.Format("Stage_{0}", StageID));
+	}
+}
