9a3ade59cb5a69de12888cbf74891ab18464294c
蒼時弦也 <elct9620@frost.tw>
2017-01-21 17:26:14 +0800

    Fix mapblock snapping problem

--- Assets/Scripts/MapBlock.cs
+++ Assets/Scripts/MapBlock.cs
@@ -52,11 +52,12 @@ public class MapBlock : MonoBehaviour {
 	}
 
 	void SnapToBlock() {
+		
 		if(transform.hasChanged) {
 			Vector3 currentPos = transform.position;
 			transform.position = new Vector3(
-				Mathf.RoundToInt(currentPos.x) * MAP_WIDTH,
-				Mathf.RoundToInt(currentPos.y) * MAP_HEIGHT,
+				Mathf.RoundToInt(currentPos.x / MAP_WIDTH) * MAP_WIDTH,
+				Mathf.RoundToInt(currentPos.y / MAP_HEIGHT) * MAP_HEIGHT,
 				0	
 			);
 		}
