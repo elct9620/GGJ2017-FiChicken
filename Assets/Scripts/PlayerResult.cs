913cbc0319b412941aa84b578f650a96fe4a788e
蒼時弦也 <elct9620@frost.tw>
2017-01-22 14:30:22 +0800

    Add Score board UI

---
+++ Assets/Scripts/PlayerResult.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerResult : MonoBehaviour {
+
+	public GameObject KilledUI;
+	public GameObject DeadUI;
+	public GameObject ScoreUI;
+
+	public PlayerResult SetPlayer(int ID) {
+		string ResourceName = string.Format("UI/Rank_{0}", ID);
+		GetComponent<Image>().sprite = Resources.Load<Sprite>(ResourceName);
+		return this;
+	}
+
+	public PlayerResult SetKill(int Kills) {
+		KilledUI.GetComponent<Text>().text = string.Format("{0}", Kills);
+		return this;
+	}
+
+	public PlayerResult SetDead(int Dead) {
+		DeadUI.GetComponent<Text>().text = string.Format("{0}", Dead);
+		return this;
+	}
+
+	public PlayerResult SetScore(int Score) {
+		ScoreUI.GetComponent<Text>().text = string.Format("{0}", Score);
+		return this;
+	}
+}
---
+++ Assets/Scripts/RankUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class RankUI : MonoBehaviour {
+
+	public GameObject PlayerResult;
+
+	void Start () {
+		// TODO: Read scores
+	}
+
+	void CreateRank(int ID, int Kill, int Dead, int Score) {
+		GameObject Result = Instantiate(PlayerResult);
+		Result.transform.parent = gameObject.transform;
+		Result.transform.localScale = new Vector3(1, 1, 1);
+		Result.GetComponent<PlayerResult>().SetPlayer(ID).SetKill(Kill).SetDead(Dead).SetScore(Score);
+	}
+
+}
